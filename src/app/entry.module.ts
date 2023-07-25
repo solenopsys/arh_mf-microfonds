be8f4e8fe8f47124b1b3544fc844d9d26a190e29
AlexStorm <info@alexstorm.ru>
2023-07-25 12:56:30 +0530

    min people

--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -11,7 +11,6 @@ import {MicrofondsComponent} from "./microfonds/microfonds.component";
             path: "",
             component: MicrofondsComponent,
         }]),
-
     ],
     providers: [],
 })
